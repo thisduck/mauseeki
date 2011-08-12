fb69d069b083ad8b765749e0990161fa64134244
Adnan Ali <adnan.ali@gmail.com>
2011-08-12 01:20:28 -0400

    initial setup.

---
+++ config/initializers/mongodb.rb
@@ -0,0 +1,8 @@
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
+else
+  logger = ENV['LOG_MONGO'] ? Rails.logger : nil
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => logger)
+  MongoMapper.database  = "mdot_#{Rails.env}"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdot::Application.config.secret_token = 'ab8d69b57c8548f21ecee0650db116f4729fb8c12a1689551bfe8fba6e2201ab54c72df581f902043643f9037463b7950332d0f9b7639930a37e21928747f738'
