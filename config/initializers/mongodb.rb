4b6de20954a9b7c61c68670815e1b62a4aa46a9b
Adnan Ali <adnan.ali@gmail.com>
2011-08-13 01:04:00 -0400

    rails env.

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,6 +1,6 @@
 if ENV['MONGOLAB_URI']
-  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
-  MongoMapper.connect(RAILS_ENV)
+  MongoMapper.config = {Rails.env => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(Rails.env)
 else
   logger = ENV['LOG_MONGO'] ? Rails.logger : nil
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => logger)
